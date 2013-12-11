c444d7fd3d10f19c53805a5b279e0f26ebba584a
Carson Full <carson.full@gmomail.org>
2013-12-11 16:50:18 -0600

    Added String case insensitive functions and PHPDocs.

--- tests/UnitTest/StringTest.php
+++ tests/UnitTest/StringTest.php
@@ -12,24 +12,34 @@ class StringTest extends \PHPUnit_Framework_TestCase {
 		$this->assertTrue(String::contains("this", "s"));
 		$this->assertTrue(String::contains("this", "th"));
 		$this->assertTrue(String::contains("this", "is"));
+		$this->assertTrue(String::iContains("this", "this"));
+
+		$this->assertTrue(String::iContains("thIs", "is"));
+		$this->assertTrue(String::iContains("this", "THIS"));
 	}
 
 	public function test_empty_returns_true() {
 		$this->assertTrue(String::contains("this", ""));
+		$this->assertTrue(String::iContains("this", ""));
 		$this->assertTrue(String::startsWith("this", ""));
+		$this->assertTrue(String::iStartsWith("this", ""));
 		$this->assertTrue(String::endsWith("this", ""));
+		$this->assertTrue(String::iEndsWith("this", ""));
 	}
 
 	public function test_does_not_contain() {
 		$this->assertFalse(String::contains("this", "q"));
 		$this->assertFalse(String::contains("this", "qthis"));
 		$this->assertFalse(String::contains("this", "thisq"));
+		$this->assertFalse(String::contains("this", "THIS"));
 	}
 
 	public function test_startsWith() {
 		$this->assertTrue(String::startsWith("blah", "b"));
 		$this->assertTrue(String::startsWith("blah", "bla"));
 		$this->assertTrue(String::startsWith("blah", "blah"));
+
+		$this->assertTrue(String::iStartsWith("bLAh", "blah"));
 	}
 
 	public function test_does_not_startsWith() {
@@ -37,12 +47,16 @@ class StringTest extends \PHPUnit_Framework_TestCase {
 		$this->assertFalse(String::startsWith("blah", "blaq"));
 		$this->assertFalse(String::startsWith("blah", "qbla"));
 		$this->assertFalse(String::startsWith("blah", "lah"));
+
+		$this->assertFalse(String::startsWith("BLah", "lah"));
 	}
 
 	public function test_endsWith() {
 		$this->assertTrue(String::endsWith("blah", "h"));
 		$this->assertTrue(String::endsWith("blah", "ah"));
 		$this->assertTrue(String::endsWith("blah", "blah"));
+
+		$this->assertTrue(String::iEndsWith("blAH", "blah"));
 	}
 
 	public function test_does_not_endsWith() {
@@ -50,6 +64,7 @@ class StringTest extends \PHPUnit_Framework_TestCase {
 		$this->assertFalse(String::endsWith("blah", "qblah"));
 		$this->assertFalse(String::endsWith("blah", "lalh"));
 		$this->assertFalse(String::endsWith("blah", "lahq"));
+		$this->assertFalse(String::endsWith("blah", "BLAH"));
 	}
 
 }
