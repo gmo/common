d4379016485b4ed22e0e83c526e492f186b93996
Carson Full <carson.full@gmomail.org>
2014-03-21 11:45:45 -0500

    Added ConsoleLogger to echo out log statement.

---
+++ src/GMO/Common/ConsoleLogger.php
@@ -0,0 +1,18 @@
+<?php
+namespace GMO\Common;
+
+use Psr\Log\AbstractLogger;
+
+class ConsoleLogger extends AbstractLogger {
+
+	/**
+	 * Logs with an arbitrary level.
+	 * @param mixed  $level
+	 * @param string $message
+	 * @param array  $context
+	 * @return null
+	 */
+	public function log($level, $message, array $context = array()) {
+		echo sprintf("%s %s: %s\n", date("Y-m-d H:i:s"), strtoupper($level), $message);
+	}
+}
