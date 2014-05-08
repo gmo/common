0b8e1b84acc4f48e9112920cd67ae226e0fa9a4d
Mike Gillis <mike.gillis@gmomail.org>
2014-05-08 15:39:50 -0500

    Change Http::getRemoteIP to the standard method for getting IPs

--- src/GMO/Common/Http.php
+++ src/GMO/Common/Http.php
@@ -13,10 +13,7 @@ class Http {
 		if(isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
 			// If the header is present, use the last IP address.
 			$temp_array = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-			$ip = $temp_array[count($temp_array) - 1];
-			if (static::isValidIp($ip)) {
-				return $ip;
-			}
+			return $temp_array[count($temp_array) - 1];
 		}
 
 		if (isset($_SERVER["REMOTE_ADDR"])) {
@@ -26,9 +23,5 @@ class Http {
 		}
 
 		return "";
-	}
-
-	private static function isValidIp($ip) {
-		return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false;
 	}
 }
