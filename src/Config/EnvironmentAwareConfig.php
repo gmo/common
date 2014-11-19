a341d0dca41f7217d34793c6abf16a1942945dd5
Carson Full <carson.full@gmomail.org>
2014-11-19 17:24:19 -0600

    Added EnvironmentAwareConfig

---
+++ src/Config/EnvironmentAwareConfig.php
@@ -0,0 +1,82 @@
+<?php
+namespace GMO\Common\Config;
+
+use GMO\Common\Collections\ArrayCollection;
+use GMO\Common\Exception\ConfigException;
+use GMO\Common\String;
+
+/**
+ * This config groups values by environment.
+ * A "default" environment is required.
+ * By default the selected environment is based on the environment variable "PHP_ENV".
+ * If the config does not contain the selected environment the default is used.
+ *
+ * Example:
+ *     default:
+ *       section:
+ *         test: hello
+ *
+ *     staging:
+ *       section:
+ *         test: world
+ *
+ *     development:
+ *       section:
+ *         test: %staging%
+ *
+ *     The result for "section > test" for all environments
+ *       production: hello
+ *       staging: world
+ *       development: world
+ */
+abstract class EnvironmentAwareConfig extends AbstractConfig {
+
+	public static function getEnvironment() { return getenv('PHP_ENV'); }
+
+	public static function getValue($section, $key, $default = null) {
+		$value = parent::getValue($section, $key, $default);
+
+		if (!String::startsWith($value, '%') || !String::endsWith($value, '%')) {
+			return $value;
+		}
+
+		$env = substr($value, 1, strlen($value) - 2);
+		if (!static::$environments->containsKey($env)) {
+			$location = static::$envName . " > " . ($section ? "$section > " : "") . "$key";
+			throw new ConfigException("Config file does not contain the environment: \"$env\" requested by $location");
+		}
+
+		static::$config = static::$environments->get($env);
+		$value = static::getValue($section, $key, $default);
+		static::$config = static::$environments->get(static::$envName);
+
+		return $value;
+	}
+
+	protected static function doSetConfig() {
+		parent::doSetConfig();
+
+		/** @var ArrayCollection|null $default */
+		if (!$default = static::$config->remove('default')) {
+			throw new ConfigException('Config needs to have a default environment');
+		}
+
+		static::$envName = static::getEnvironment();
+
+		// Build config for each environment merging default values in
+		static::$environments = new ArrayCollection();
+		foreach (static::$config as $environment => $envConfig) {
+			static::$environments[$environment] = $default->copy()->replaceRecursive($envConfig);
+		}
+
+		if (!static::$environments->containsKey(static::$envName)) {
+			static::$environments[static::$envName] = $default;
+		}
+
+		static::$config = static::$environments[static::$envName];
+	}
+
+	protected static $envName;
+	/** @var ArrayCollection[]|ArrayCollection */
+	protected static $environments;
+}
---
+++ tests/UnitTest/Config/EnvironmentAwareConfigTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace UnitTest\Config;
+
+use GMO\Common\Config\EnvironmentAwareConfig;
+
+class EnvironmentAwareConfigTest extends \PHPUnit_Framework_TestCase {
+
+	public function testDefaultEnvironment() {
+		TestConfig::setEnvironment('production');
+		$foo = TestConfig::getValue('test', 'foo');
+		$this->assertSame('bar', $foo);
+	}
+
+	public function testSpecificEnvironment() {
+		TestConfig::setEnvironment('production');
+		$value = TestConfig::getValue('test', 'hello');
+		$this->assertSame('world2', $value);
+	}
+
+	public function testUnknownEnvironmentUsesDefault() {
+		TestConfig::setEnvironment('asdf');
+		$value = TestConfig::getValue('test', 'hello');
+		$this->assertSame('world', $value);
+	}
+
+	public function testGettingValueFromAliasedEnvironment() {
+		TestConfig::setEnvironment('development');
+		$value = TestConfig::getValue('test', 'hello');
+		$this->assertSame('world2', $value);
+	}
+
+	/**
+	 * @expectedException \GMO\Common\Exception\ConfigException
+	 */
+	public function testUnknownAliasedEnvironment() {
+		TestConfig::setEnvironment('development');
+		TestConfig::getValue('test', 'error');
+	}
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		static::$originalEnvironment = getenv('PHP_ENV');
+	}
+
+	public static function tearDownAfterClass() {
+		putenv('PHP_ENV=' . static::$originalEnvironment);
+		parent::tearDownAfterClass();
+	}
+
+	static $originalEnvironment;
+}
+
+
+class TestConfig extends EnvironmentAwareConfig {
+
+	public static function setEnvironment($env) {
+		putenv("PHP_ENV=$env");
+		static::doSetConfig();
+	}
+
+	public static function setProjectDir() { return "../../.."; }
+	public static function setConfigFile() { return "tests/testConfig.yml"; }
+}
