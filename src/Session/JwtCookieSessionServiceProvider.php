4e1c47798e0833460632d108217da084f9c56584
Carson Full <carson.full@gmomail.org>
2015-01-19 12:21:33 -0600

    Added service provider for jwt cookie session.

---
+++ src/Session/JwtCookieSessionServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace GMO\Common\Session;
+
+use Silex\Application;
+use Silex\Provider\SessionServiceProvider;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class JwtCookieSessionServiceProvider extends SessionServiceProvider {
+
+	public function register(Application $app) {
+		parent::register($app);
+
+		$app['session.storage'] = $app->share(function($app) {
+			return new JwtCookieSessionStorage(
+				$app['session.storage.cookie.name'],
+				$app['session.storage.cookie.secret']
+			);
+		});
+
+		$app['session.bag.attribute'] = function($app) {
+			return new AutoSavingAttributeBag($app['session.storage']);
+		};
+
+		$app['session'] = $app->share(function($app) {
+			return new Session($app['session.storage'], $app['session.bag.attribute']);
+		});
+	}
+}
