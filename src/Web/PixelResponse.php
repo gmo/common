997f1814154fa88c80190f1080aef9f20424efd1
Carson Full <carson.full@gmomail.org>
2015-04-20 16:12:39 -0500

    Added PixelResponse. A pixel.gif that's been adapted to a Response object.

---
+++ src/Web/PixelResponse.php
@@ -0,0 +1,28 @@
+<?php
+namespace GMO\Common\Web;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * A transparent pixel response
+ */
+class PixelResponse extends Response {
+
+	/**
+	 * Base64 encoded transparent pixel
+	 */
+	const PIXEL = 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
+	public function __construct() {
+		parent::__construct(null);
+
+		$this->content = base64_decode(static::PIXEL);
+		$this->headers->set('Content-Type', 'image/gif');
+	}
+
+	public function setContent($content) {
+		if ($content !== null) {
+			throw new \LogicException('The content cannot be set on a PixelResponse instance.');
+		}
+	}
+}
