902e614c732a2b80bb86a299262e2879db461d74
Carson Full <carson.full@gmomail.org>
2015-04-20 16:09:33 -0500

    Added Env and Request Log Processors and DelegateLogger

---
+++ src/Log/Processor/EnvProcessor.php
@@ -0,0 +1,16 @@
+<?php
+namespace GMO\Common\Log\Processor;
+
+class EnvProcessor {
+
+	public function __invoke(array $record) {
+		$record['extra']['env'] = $this->env;
+		return $record;
+	}
+
+	public function __construct($env) {
+		$this->env = $env;
+	}
+
+	protected $env;
+}
