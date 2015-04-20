3b16739bb58ca02d3bf62a2faec24245e9e0ada1
Carson Full <carson.full@gmomail.org>
2015-04-20 16:09:33 -0500

    Added Env and Request Log Processors and DelegateLogger

---
+++ src/Log/DelegateLogger.php
@@ -0,0 +1,26 @@
+<?php
+namespace Gmo\Common\Log;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Delegates logging to a given logger.
+ * Allows a real logger to be swapped in after this logger is given to an object.
+ */
+class DelegateLogger extends AbstractLogger implements LoggerAwareInterface {
+
+	public function setLogger(LoggerInterface $logger) {
+		$this->logger = $logger;
+	}
+
+	public function log($level, $message, array $context = array()) {
+		if ($this->logger) {
+			$this->logger->log($level, $message, $context);
+		}
+	}
+
+	/** @var LoggerInterface|null */
+	protected $logger;
+}
---
+++ src/Log/Processor/RequestProcessor.php
@@ -0,0 +1,36 @@
+<?php
+namespace Gmo\Common\Log\Processor;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class RequestProcessor {
+
+	public function __invoke(array $record) {
+		if (!$request = $this->requestStack->getCurrentRequest()) {
+			return $record;
+		}
+
+		$params = [
+			'method' => $request->getMethod(),
+			'host' => $request->getHost(),
+			'path' =>  $request->getPathInfo(),
+			'query' => $request->query->all(),
+			'userAgent' => $request->server->get('HTTP_USER_AGENT'),
+		];
+		if ($request->isMethod('POST')) {
+			$params['body'] = $request->request->all();
+		}
+		if ($referer = $request->headers->get('referer')) {
+			$params['referer'] = $referer;
+		}
+
+		$record['extra']['request'] = $params;
+		return $record;
+	}
+
+	public function __construct(RequestStack $requestStack) {
+		$this->requestStack = $requestStack;
+	}
+
+	protected $requestStack;
+}
