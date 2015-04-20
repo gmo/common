107abbaf147a4cb0f054c6b8a1e123ea10632667
Carson Full <carson.full@gmomail.org>
2015-04-20 16:41:14 -0500

    Added extended ControllerCollection.

---
+++ src/Routing/DefaultControllerAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace Gmo\Common\Routing;
+
+interface DefaultControllerAwareInterface {
+
+	/**
+	 * Sets the default controller class.
+	 *
+	 * This is this first part of a callable so it can be a string or an object.
+	 *
+	 * @param string|object $class
+	 */
+	public function setDefaultControllerClass($class);
+
+	/**
+	 * Sets the default controller class method.
+	 *
+	 * This is this second part of a callable.
+	 *
+	 * @param string $method
+	 */
+	public function setDefaultControllerMethod($method);
+}
