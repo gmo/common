d67dbaff460e9cc149dc2953d9b10ae3da9bee37
Carson Full <carson.full@gmomail.org>
2015-04-20 16:12:00 -0500

    Added UrlMatcher which handles trailing slashes without redirecting

---
+++ src/Web/Routing/UrlMatcher.php
@@ -0,0 +1,38 @@
+<?php
+namespace Routing;
+
+use Silex\RedirectableUrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+/**
+ * Matches routes with or without trailing slash but does not redirect for performance
+ */
+class UrlMatcher extends RedirectableUrlMatcher {
+
+	public function match($pathinfo) {
+		try {
+			return parent::match($pathinfo);
+		} catch (ResourceNotFoundException $e) {
+			if (!in_array($this->context->getMethod(), array('HEAD', 'GET'))) {
+				throw $e;
+			}
+		}
+
+		// Try matching the route with trailing slash
+		if ('/' !== substr($pathinfo, -1)) {
+			try {
+				return parent::match($pathinfo.'/');
+			} catch (ResourceNotFoundException $e2) {
+				throw $e;
+			}
+		}
+
+		// Try matching the route without trailing slash
+		$withoutTrailingSlash = substr($pathinfo, 0, -1);
+		try {
+			return parent::match($withoutTrailingSlash);
+		} catch (ResourceNotFoundException $e2) {
+			throw $e;
+		}
+	}
+}
