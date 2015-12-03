7e6d6d6f51bc40a5bb06bc644396decf93cb7603
Carson Full <carson.full@gmomail.org>
2015-12-03 17:47:51 -0600

    [UrlMatcher] Fix bug where a route with an omitted optional variable would not get matched. Fixed "_prefixed_route" not being removed from route attributes. Added tests! Class has full coverage now.

--- src/Web/Routing/UrlMatcher.php
+++ src/Web/Routing/UrlMatcher.php
@@ -1,41 +1,63 @@
 <?php
 namespace GMO\Common\Web\Routing;
 
+use Silex\RedirectableUrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher as UrlMatcherBase;
 use Symfony\Component\Routing\Route;
 
 /**
  * {@inheritdoc}
  *
- * Trailing slash is removed before matching.
+ * Matches routes with or without trailing slash but does not redirect for performance.
  */
-class UrlMatcher extends UrlMatcherBase {
+class UrlMatcher extends RedirectableUrlMatcher
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function match($pathinfo)
+    {
+        // Try matching the route without trailing slash first as this is likely to be the case.
+        $pathinfo = rtrim($pathinfo, '/');
+        try {
+            /*
+             * RedirectableUrlMatcher attempts to match a trailing slash
+             * on matching failure and then (if found) redirect to it.
+             * We want to skip the redirect and just match it in this
+             * request for performance. So our parent calls skip this
+             * class and go straight to Symfony's UrlMatcher. We still
+             * want to extend this class though, since it will redirect
+             * for mismatched scheme requirements.
+             */
+            return UrlMatcherBase::match($pathinfo);
+        } catch (ResourceNotFoundException $e) {
+        }
 
-	public function match($pathinfo) {
-		// Remove trailing slash
-		if ($pathinfo !== '/') {
-			$pathinfo = rtrim($pathinfo, '/');
-		}
+        // Next try with trailing slash
+        try {
+            return UrlMatcherBase::match($pathinfo . '/');
+        } catch (ResourceNotFoundException $e2) {
+            throw $e;
+        }
+    }
 
-		return parent::match($pathinfo);
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * Overrides the route name of unprefixed routes with the original/prefixed route name
+     *
+     * @see PrefixedVariableControllerCollection
+     */
+    protected function getAttributes(Route $route, $name, array $attributes)
+    {
+        $attrs = parent::getAttributes($route, $name, $attributes);
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * Overrides the route name of unprefixed routes with the original/prefixed route name
-	 *
-	 * @see PrefixedVariableControllerCollection
-	 */
-	protected function getAttributes(Route $route, $name, array $attributes) {
-		$attrs = parent::getAttributes($route, $name, $attributes);
+        if (isset($attrs['_prefixed_route'])) {
+            $attrs['_route'] = $attrs['_prefixed_route'];
+            unset($attrs['_prefixed_route']);
+        }
 
-		if (isset($attrs['_prefixed_route'])) {
-			$name = $attrs['_prefixed_route'];
-			$attrs['_route'] = $name;
-			unset($attributes['_prefixed_route']);
-		}
-
-		return $attrs;
-	}
+        return $attrs;
+    }
 }
