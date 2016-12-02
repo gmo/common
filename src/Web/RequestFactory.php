ea3097f960814791e359ab7d937ae5f9840f9ac1
Carson Full <carsonfull@gmail.com>
2016-12-02 10:58:40 -0600

    Update RequestFactory to accept options

--- src/Web/RequestFactory.php
+++ src/Web/RequestFactory.php
@@ -2,37 +2,53 @@
 
 namespace GMO\Common\Web;
 
-class RequestFactory {
+class RequestFactory
+{
+    protected $options = array();
 
-	/**
-	 * Registers this as the factory to use when creating requests.
-	 */
-	public static function register() {
-		Request::setFactory(array(get_called_class(), 'factory'));
-	}
+    /**
+     * Constructor.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        $this->options = $options;
+    }
 
-	/**
-	 * Creates a new request with the given parameters.
-	 *
-	 * @param array $query
-	 * @param array $request
-	 * @param array $attributes
-	 * @param array $cookies
-	 * @param array $files
-	 * @param array $server
-	 * @param null  $content
-	 *
-	 * @return Request
-	 */
-	public static function factory(
-		array $query = array(),
-		array $request = array(),
-		array $attributes = array(),
-		array $cookies = array(),
-		array $files = array(),
-		array $server = array(),
-		$content = null
-	) {
-		return new Request($query, $request, $attributes, $cookies, $files, $server, $content);
-	}
+    /**
+     * Registers this as the factory to use when creating requests.
+     *
+     * @param array $options
+     */
+    public static function register($options = array())
+    {
+        $factory = new static($options);
+        Request::setFactory(array($factory, 'createRequest'));
+    }
+
+    /**
+     * Creates a new request with the given parameters.
+     *
+     * @param array           $query
+     * @param array           $request
+     * @param array           $attributes
+     * @param array           $cookies
+     * @param array           $files
+     * @param array           $server
+     * @param string|resource $content
+     *
+     * @return Request
+     */
+    public function createRequest(
+        array $query = array(),
+        array $request = array(),
+        array $attributes = array(),
+        array $cookies = array(),
+        array $files = array(),
+        array $server = array(),
+        $content = null
+    ) {
+        return new Request($query, $request, $attributes, $cookies, $files, $server, $content);
+    }
 }
