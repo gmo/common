c7bf9f0e420390b78982c09a361430535a557a84
Carson Full <carsonfull@gmail.com>
2017-07-17 19:43:01 -0500

    Added ContainerAwareTrait::getOrCreate() to help with commands where the container is optional.

--- src/Console/ContainerAwareTrait.php
+++ src/Console/ContainerAwareTrait.php
@@ -12,6 +12,28 @@ use Symfony\Component\Console\Command\Command;
  */
 trait ContainerAwareTrait
 {
+    /**
+     * Get a service by ID from the container if it exists or create it with the callable.
+     *
+     * @param string   $id
+     * @param callable $creator
+     *
+     * @return mixed
+     */
+    protected function getOrCreate(string $id, callable $creator)
+    {
+        if (!$this->getHelperSet()->has('container')) {
+            return $creator();
+        }
+
+        $container = $this->getContainer();
+        if ($container->has($id)) {
+            return $container->get($id);
+        }
+
+        return $creator();
+    }
+
     protected function getContainer(): ContainerInterface
     {
         return $this->getHelper('container');
